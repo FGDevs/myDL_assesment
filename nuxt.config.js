b0eb1aa3e309188b8b0894dbff34ce136126658f
fitrah_gitlab <fitrakadev@gmail.com>
2021-11-19 15:36:04 +0700

    Config: Typescript & Custom Import Path

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,13 @@
+import { resolve } from 'path'
+
 export default {
   target: 'server',
   buildModules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/typescript-build'
-  ]
+  ],
+  srcDir: 'src/',
+  alias: {
+    'components': resolve(__dirname, './src/components')
+  }
 }
---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts"> 
+import { defineComponent } from '@vue/composition-api'
+import TheHeader from 'components/TheHeader.vue'
+
+export default defineComponent({
+  name: 'HomePage',
+  components: { TheHeader }
+})
+</script>
